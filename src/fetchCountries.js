eafd1f61ddc00c5ccdd5d6fdeed2cc51da754f70
Maryna-Hemb <marinagembarskaa@gmail.com>
2023-01-13 02:02:55 +0200

    feat task

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,12 +1,14 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio'
+
 export function fetchCountries(name) {
     const BASE_URL = "https://restcountries.com/v2/name/";
     const FIELDS = "name,capital,population,flag,languages"
   return  fetch(`${BASE_URL}${name}?fields=${FIELDS}`)
       .then(resp => {
-          console.log(resp);
-          if (!resp.ok) {
-            throw new Error(resp.statusText)
-        }
-            return resp.json()
+              if (!resp.ok) {
+              throw new Error(resp.statusText)
+           
+          }
+          return resp.json()
         })
 }
--- src/index.js
+++ src/index.js
@@ -8,30 +8,70 @@ const inputEl = document.querySelector("#search-box");
 const listEl = document.querySelector(".country-list");
 const infoEl = document.querySelector(".country-info");
 
+
+
 inputEl.addEventListener("input", debounce(onInput, DEBOUNCE_DELAY, { leading: false,
-      trailing: true,}))
+    trailing: true,
+}))
+      
 
 function onInput(evt) {
-   
-     const name = inputEl.value 
-    
-    console.log(name)
-
-    
-    
-    fetchCountries(name).then(data => {
-        console.log(data.length);
+               
+    const name = inputEl.value.trim() 
+       
+    if (name.length === 0) { cleanInformarion();  return }
+       
+    fetchCountries(name)
+        .then(data => {
         if (data.length > 10) {
             return Notify.info("Too many matches found. Please enter a more specific name.")
         }
-        // if(data.length > 1) {listEl.innerHTML=}
-    })
-//     .then(data => list.innerHTML = createMarkup(data.forecast.forecastday))
-//     .catch(err => list.innerHTML = createErrorMarkup())
-    
+        else if (data.length > 1) { listEl.innerHTML = createMarkupList(data) }
+        else if (data.length === 1) { infoEl.innerHTML = createMarkupInfo(data) }
+        })    
+        .catch(err => { cleanInformarion();  createErrorMarkup()})    
+  
 }
 
+function createMarkupList(arr) {
+    infoEl.innerHTML = "";
+    return arr.map(({
+           name,
+            flag
+        }) =>
+        `<li class="list-item">
+        <img src="${flag}" alt="flag class="list-item__flag style="width:40px; height:auto">
+        <h2 class="list-item__country">${name}</h2>
+        </li>`
+    ).join('')    
+}
 
+function createMarkupInfo(arr) {
+    listEl.innerHTML = "";    
+    return arr.map(({
+           name,
+        flag,
+        capital,
+        population,
+        languages : {0: {name : languages}}
+        }) =>
+        `<div class="list-item">
+        <img src="${flag}" alt="flag class="list-item__flag style="width:40px; height:auto">
+        <h2>${name}</h2>
+        </div>
+        <h3>Capital:<span>"${capital}"</span></h3>
+        <h3>Population:<span>"${population}"</span></h3>
+        <h3>Languages:<span>"${languages}"</span></h3>
+        `
+    ).join('')    
+}
 
+function createErrorMarkup(){
+    return Notify.failure("Oops, there is no country with that name")
+}
 
+function cleanInformarion() {
+    listEl.innerHTML = "";
+    infoEl.innerHTML = ""
+}
 
